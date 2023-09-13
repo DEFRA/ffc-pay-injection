af756cff30ae51326f60899ef3620710cb9a01eb
John Watson <john.watson1@defra.gov.uk>
2023-09-13 12:42:43 +0100

    Add tests

---
+++ test/unit/insights.test.js
@@ -0,0 +1,42 @@
+describe('Application Insights', () => {
+  const DEFAULT_ENV = process.env
+  let applicationInsights
+
+  beforeEach(() => {
+    // important to clear the cache when mocking environment variables
+    jest.resetModules()
+    jest.mock('applicationinsights', () => {
+      return {
+        setup: jest.fn().mockReturnThis(),
+        setAutoCollectDependencies: jest.fn().mockReturnThis(),
+        start: jest.fn(),
+        defaultClient: {
+          context: {
+            keys: [],
+            tags: []
+          }
+        }
+      }
+    })
+    applicationInsights = require('applicationinsights')
+    process.env = { ...DEFAULT_ENV }
+  })
+
+  afterAll(() => {
+    process.env = DEFAULT_ENV
+  })
+
+  test('does not setup application insights if no instrumentation key', () => {
+    process.env.APPINSIGHTS_CONNECTIONSTRING = undefined
+    const appInsights = require('../../app/insights')
+    appInsights.setup()
+    expect(applicationInsights.setup.mock.calls.length).toBe(0)
+  })
+
+  test('does setup application insights if instrumentation key present', () => {
+    const appInsights = require('../../app/insights')
+    process.env.APPINSIGHTS_CONNECTIONSTRING = 'test-key'
+    appInsights.setup()
+    expect(applicationInsights.setup.mock.calls.length).toBe(1)
+  })
+})
