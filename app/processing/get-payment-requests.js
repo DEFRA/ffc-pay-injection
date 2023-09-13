f330fbf4ca66fa8cbb33fdac8413cf57b85d7604
John Watson <john.watson1@defra.gov.uk>
2023-09-13 14:58:52 +0100

    Refactor parsing

---
+++ app/processing/get-payment-requests.js
@@ -0,0 +1,34 @@
+const { convertToPence } = require('../currency-convert')
+const { getDescription } = require('./get-description')
+const { getSchemeId } = require('./get-scheme-id')
+
+const getPaymentRequests = (csv) => {
+  return [...csv.filter(x => x).reduce((x, y) => {
+    const values = y.split(',')
+    const schemeId = getSchemeId(values[0])
+    const key = `${schemeId}-${values[1]}-${values[2]}-${values[3]}`
+
+    // if key doesn't exist then first instance so create new group
+    const item = x.get(key) || ({
+      schemeId,
+      frn: values[1],
+      marketingYear: values[2],
+      agreementNumber: values[3],
+      dueDate: values[7],
+      invoiceLines: [],
+      value: 0
+    })
+    item.value += convertToPence(values[6])
+    item.invoiceLines.push({
+      schemeCode: values[4],
+      description: getDescription(values[5]),
+      value: values[6]
+    })
+
+    return x.set(key, item)
+  }, new Map()).values()]
+}
+
+module.exports = {
+  getPaymentRequests
+}
--- app/processing/parse-payment-file.js
+++ app/processing/parse-payment-file.js
@@ -2,37 +2,13 @@ const moment = require('moment')
 const { v4: uuidv4 } = require('uuid')
 const { GBP } = require('../constants/currency')
 const { INJECTION } = require('../constants/source-system')
-const { getSchemeId } = require('./get-scheme-id')
-const { getDescription } = require('./get-description')
-const { convertToPence, convertToPounds } = require('../currency-convert')
+const { convertToPounds } = require('../currency-convert')
 const { createInvoiceNumber } = require('./create-invoice-number')
+const { getPaymentRequests } = require('./get-payment-requests')
 
 const parsePaymentFile = async (data, filename, transaction) => {
   const csv = data.trim().split(/\r?\n/)
-  const paymentRequests = [...csv.filter(x => x).reduce((x, y) => {
-    const values = y.split(',')
-    const schemeId = getSchemeId(values[0])
-    const key = `${schemeId}-${values[1]}-${values[2]}-${values[3]}`
-
-    // if key doesn't exist then first instance so create new group
-    const item = x.get(key) || ({
-      schemeId,
-      frn: values[1],
-      marketingYear: values[2],
-      agreementNumber: values[3],
-      dueDate: values[7],
-      invoiceLines: [],
-      value: 0
-    })
-    item.value += convertToPence(values[6])
-    item.invoiceLines.push({
-      schemeCode: values[4],
-      description: getDescription(values[5]),
-      value: values[6]
-    })
-
-    return x.set(key, item)
-  }, new Map()).values()]
+  const paymentRequests = getPaymentRequests(csv)
 
   for (const paymentRequest of paymentRequests) {
     paymentRequest.sourceSystem = INJECTION
