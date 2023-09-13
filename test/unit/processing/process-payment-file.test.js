99036f54105d4ceb048a81c845fe813600215386
John Watson <john.watson1@defra.gov.uk>
2023-09-13 14:56:24 +0100

    Add tests

---
+++ test/unit/processing/process-payment-file.test.js
@@ -0,0 +1,66 @@
+jest.mock('../../../app/storage')
+const { downloadFile, archiveFile } = require('../../../app/storage')
+
+jest.mock('../../../app/processing/parse-payment-file')
+const { parsePaymentFile } = require('../../../app/processing/parse-payment-file')
+
+jest.mock('../../../app/processing/quarantine-file')
+const { quarantineFile } = require('../../../app/processing/quarantine-file')
+
+jest.mock('../../../app/messaging')
+const { sendPaymentMessages } = require('../../../app/messaging')
+
+const { processPaymentFile } = require('../../../app/processing/process-payment-file')
+
+const filename = 'FFCS_Manual_Batch_20230913140000.csv'
+
+describe('process payment file', () => {
+  beforeEach(async () => {
+    jest.clearAllMocks()
+    parsePaymentFile.mockResolvedValue([{ schemeId: 1 }])
+  })
+
+  test('should download file', async () => {
+    await processPaymentFile(filename)
+    expect(downloadFile).toHaveBeenCalledWith(filename)
+  })
+
+  test('should parse file', async () => {
+    await processPaymentFile(filename)
+    expect(parsePaymentFile).toHaveBeenCalled()
+  })
+
+  test('should quarantine file if parse fails', async () => {
+    parsePaymentFile.mockImplementation(() => {
+      throw new Error('Parse error')
+    })
+    await processPaymentFile(filename)
+    expect(quarantineFile).toHaveBeenCalledWith(filename, new Error('Parse error'))
+  })
+
+  test('should archive file if parse succeeds', async () => {
+    await processPaymentFile(filename)
+    expect(archiveFile).toHaveBeenCalledWith(filename)
+  })
+
+  test('should not archive file if parse fails', async () => {
+    parsePaymentFile.mockImplementation(() => {
+      throw new Error('Parse error')
+    })
+    await processPaymentFile(filename)
+    expect(archiveFile).not.toHaveBeenCalled()
+  })
+
+  test('should not send messages if parse fails', async () => {
+    parsePaymentFile.mockImplementation(() => {
+      throw new Error('Parse error')
+    })
+    await processPaymentFile(filename)
+    expect(sendPaymentMessages).not.toHaveBeenCalled()
+  })
+
+  test('should send messages if parse succeeds', async () => {
+    await processPaymentFile(filename)
+    expect(sendPaymentMessages).toHaveBeenCalled()
+  })
+})
