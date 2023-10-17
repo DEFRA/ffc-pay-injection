ad9c8d1a3c65f7dd17a506e3a6768f86e0fcade9
John Watson <john.watson1@defra.gov.uk>
2023-10-17 09:53:23 +0100

    Change invoice number format

--- app/processing/create-invoice-number.js
+++ app/processing/create-invoice-number.js
@@ -2,7 +2,7 @@ const db = require('../data')
 
 const createInvoiceNumber = async (paymentRequest, transaction) => {
   const invoiceNumberRecord = await db.invoiceNumber.create({ ...paymentRequest, created: new Date() }, { transaction })
-  return `X${invoiceNumberRecord.invoiceId.toString().padStart(8, '0')}${paymentRequest.agreementNumber}V000`
+  return `X${invoiceNumberRecord.invoiceId.toString().padStart(7, '0')}${paymentRequest.agreementNumber}V000`
 }
 
 module.exports = {
