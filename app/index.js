e33819472d1357b9634553cdaa506b9a6f4f06e2
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2025-03-14 14:23:37 +0000

    add liveness checks (#27)
    * add liveness checks
    
    * 1.2.5
    
    ---------
    
    Co-authored-by: ffcplatform <ffcplatforms@environment-agency.gov.uk>

--- app/index.js
+++ app/index.js
@@ -3,8 +3,10 @@ require('./insights').setup()
 
 const config = require('./config')
 const processing = require('./processing')
+const { start: startServer } = require('./server')
 
 const startApp = async () => {
+  await startServer()
   if (config.processingActive) {
     await processing.start()
   } else {
